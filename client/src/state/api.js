73b24a8b50776de1256f60482965084b679eb32d
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-09-21 00:33:27 -0700

    done

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://admin-server-98to.onrender.com" }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
